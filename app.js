793dfa91185d5547af467793271db61e43af14c4
omkarSahebpatil <151365781+omkarSahebpatil@users.noreply.github.com>
2023-11-18 03:02:10 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express= require("express");
 const socket = require("socket.io");
 const app=express();  
 
-app.use(express.static("/public"));
+
+app.get("/", (req,res)=>{
+    res.sendFile("/index.html", {root: __dirname});
+})
 
 let port = process.env.PORT || 5000;
 
