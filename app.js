115e8aab07ec1c6a3d7a5d9cf2701e6cf05941ad
omkarSahebpatil <151365781+omkarSahebpatil@users.noreply.github.com>
2023-11-18 02:33:27 -0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app=express();
 
 app.use(express.static("public"));
 
-let port = 5000;
+let port = process.env.PORT || 5000;
 
 let Server = app.listen(port, () =>{
     console.log("listening to port "+port);
